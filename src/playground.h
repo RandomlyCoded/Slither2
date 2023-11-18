bdea01f325b261e4809903d9442527951dad7db3
RandomlyCoded <marc+github@taschenorakel.de>
2023-11-18 12:09:51 +0100

    fixing playground.cpp

--- src/playground.h
+++ src/playground.h
@@ -29,89 +29,6 @@ struct EnergyPearl
     Q_PROPERTY(QColor color MEMBER color CONSTANT FINAL)
 };
 
-template<class T>
-class DataListModel : public QAbstractListModel
-{
-public:
-    enum Roles {
-        DataRole = Qt::UserRole + 1
-    };
-
-    using QAbstractListModel::QAbstractListModel;
-
-    typename QList<T>::Iterator begin() { return m_rows.begin(); }
-    typename QList<T>::Iterator end() { return m_rows.end(); }
-
-    void reset(QList<T> rows = {});
-    void add(T rows = {});
-    void remove(QModelIndex at);
-    T &at(int index);
-
-public: // QAbstractItemModel interface
-    int rowCount(const QModelIndex &parent = {}) const override;
-    QVariant data(const QModelIndex &index, int role = DataRole) const override;
-    bool setData(const QModelIndex &index, const QVariant &data, int role = DataRole) override;
-    QHash<int, QByteArray> roleNames() const override { return {{DataRole, "data"}}; }
-
-protected:
-    QList<T> m_rows;
-};
-
-template <class T>
-T &DataListModel<T>::at(int index)
-{
-    return m_rows[index];
-}
-template<class T>
-void DataListModel<T>::reset(QList<T> rows)
-{
-    beginResetModel();
-    std::swap(m_rows, rows);
-    endResetModel();
-}
-
-template<class T>
-void DataListModel<T>::add(T t)
-{
-    m_rows.append(t);
-}
-
-template<class T>
-void DataListModel<T>::remove(QModelIndex at)
-{
-    m_rows.removeAt(at.row());
-}
-
-template<class T>
-int DataListModel<T>::rowCount(const QModelIndex &parent) const
-{
-    if (parent.isValid())
-        return 0;
-
-    return m_rows.count();
-}
-
-template<class T>
-QVariant DataListModel<T>::data(const QModelIndex &index, int role) const
-{
-    if (checkIndex(index) && role == DataRole)
-        return QVariant::fromValue(m_rows[index.row()]);
-
-    return {};
-}
-
-template<class T>
-bool DataListModel<T>::setData(const QModelIndex &index, const QVariant &data, int role)
-{
-    if (checkIndex(index) && role == DataRole && data.canConvert<T>()) {
-        m_rows[index.row()] = data.value<T>();
-        emit dataChanged(index, index, {role});
-        return true;
-    }
-
-    return false;
-}
-
 class EnergyPearlListModel : public DataListModel<EnergyPearl>
 {
     Q_OBJECT;
@@ -119,6 +36,15 @@ class EnergyPearlListModel : public DataListModel<EnergyPearl>
 public:
     using DataListModel::DataListModel;
     EnergyPearlListModel(EnergyPearlListModel &other) { m_rows = other.m_rows; }
+};
+
+class SnakeListModel : public DataListModel<EnergyPearl>
+{
+    Q_OBJECT;
+
+public:
+    using DataListModel::DataListModel;
+    SnakeListModel(SnakeListModel &other) { m_rows = other.m_rows; }
 };
 
 class Playground : public QObject
